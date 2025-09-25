8ca969b7470651a26bff675b6600b42ff9cee6b2
Bueezi <66019436+Bueezi@users.noreply.github.com>
2025-09-25 19:29:12 +0200

    Update void.sh

--- void.sh
+++ void.sh
@@ -2,3 +2,5 @@
 sudo xbps-install -Su linux-firmware-amd mesa-dri vulkan-loader mesa-vulkan-radeon mesa-vaapi mesa-vdpau
 # basic gnome install
 sudo xbps-install -Su gnome-core alacritty baobab gnome-calculator gnome-tweaks loupe power-profiles-daemon
+
+enable dbus, gdm and networkmanager
