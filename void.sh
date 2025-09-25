c90c51d6af94a8b3ba5e0da5a25bc2cddcd4a9ca
Bueezi <66019436+Bueezi@users.noreply.github.com>
2025-09-25 18:12:47 +0200

    Create void.sh

---
+++ void.sh
@@ -0,0 +1,4 @@
+# amd iGPU driver
+sudo xbps-install -Su linux-firmware-amd mesa-dri vulkan-loader mesa-vulkan-radeon mesa-vaapi mesa-vdpau
+# basic gnome install
+sudo xbps-install -Su gnome-core alacritty baobab gnome-calculator gnome-tweaks loupe power-profiles-daemon
